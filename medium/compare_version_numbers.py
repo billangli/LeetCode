12b33a374bf65460105f5ae03525b6f130595b41
Bill Ang Li <bill.ang.li@hotmail.com>
2020-09-09 11:36:25 -0400

    Finish compare version strings

--- medium/compare_version_numbers.py
+++ medium/compare_version_numbers.py
@@ -1,21 +1,11 @@
 class Solution:
-    def index(self, string: str, substring: str) -> int:
-        if substring not in string:
-            return len(string)
-        return string.index(substring)
-    
     def compareVersion(self, version1: str, version2: str) -> int:
-        dot1 = self.index(version1, '.')
-        dot2 = self.index(version2, '.')
-        num1 = int(version1[:dot1])
-        num2 = int(version2[:dot2])
-        if num1 > num2:
-            return 1
-        elif num1 < num2:
-            return -1
-        elif dot1 == len(version1) and dot2 == len(version2):
-            return 0
-        else:
-            next_version1 = version1[dot1+1:] if dot1 != len(version1) else '0'
-            next_version2 = version2[dot2+1:] if dot2 != len(version2) else '0'
-            return self.compareVersion(next_version1, next_version2)
+        nums1, nums2 = version1.split('.'), version2.split('.')
+        counter = 0
+        while counter < max(len(nums1), len(nums2)):
+            n1 = int(nums1[counter]) if counter < len(nums1) else 0
+            n2 = int(nums2[counter]) if counter < len(nums2) else 0
+            if n1 < n2: return -1
+            if n1 > n2: return 1
+            counter += 1
+        return 0
